ebb223839ed9c845bc1191e00c4bf01f9736dfac
AntonioPalmaDev <antoniopalma18@outlook.com>
2025-04-22 23:31:05 -0300

    Corrigidos erros de cor e algumas funcktion js e  adc novas skills

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ const links = menu.querySelectorAll('a');
 const toggleTema = document.getElementById('toggleTema');
 const logo = document.getElementById('logo');
 const icone = toggleTema.querySelector('.icone');
+const photoshopIcon = document.getElementById('photoshopIcon');
+
+const githubIcon = document.getElementById('githubIcon');
 
 
 function trocarLogo(tema) {
+ 
   if (tema === 'light') {
+      githubIcon.src = 'img/github-original.svg'
+    photoshopIcon.src = 'img/photoshop-plain.svg'
     logo.src = 'img/logo_branca2.png'; 
   } else {
     logo.src = 'img/logo_preta3.png';
+     githubIcon.src = 'img/github-black.svg'
+    photoshopIcon.src = 'img/photoshop-black.svg'
   }
+  console.log('tema atual',tema);
 }
 
 
 hamburguer.addEventListener('click', () => {
   menu.classList.toggle('ativo');
+  
 });
 
 
@@ -71,10 +81,18 @@ const observer = new IntersectionObserver((entries) => {
     }
   };
 
+  document.addEventListener('click', (e) => {
+    const clicouForaDoMenu = !menu.contains(e.target) && !hamburguer.contains(e.target);
+    if (clicouForaDoMenu && menu.classList.contains('ativo')) {
+      menu.classList.remove('ativo');
+    }
+  });
   
   btn.addEventListener("click", function () {
     window.scrollTo({
       top: 0,
       behavior: "smooth"
     });
+    
   });
+ 
